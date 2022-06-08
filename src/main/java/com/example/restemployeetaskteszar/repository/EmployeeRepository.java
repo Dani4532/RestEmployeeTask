0142683c5531a4af64d8844f8c795bca4cfdbab7
Dani4532 <d.teszar@htlstp.at>
2022-06-08 22:03:22 +0200

    getTasksFromTo getHouresWorked

--- src/main/java/com/example/restemployeetaskteszar/repository/EmployeeRepository.java
+++ src/main/java/com/example/restemployeetaskteszar/repository/EmployeeRepository.java
@@ -1,10 +1,30 @@
 package com.example.restemployeetaskteszar.repository;
 
 import com.example.restemployeetaskteszar.entity.Employee;
+import com.example.restemployeetaskteszar.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
     List<Employee> findEmployeesByFirstNameContaining(String name);
+
+    @Query("""
+            select task
+            from Task task
+            where task.employee.id = ?1 and
+            task.finished > ?2 and 
+            task.finished < ?3
+            """)
+    List<Task> tasksFinishedByEmployee(String id, LocalDate from, LocalDate to);
+
+    @Query("""
+            select sum(task.houresWorked)
+            from Task task
+            where task.employee.id = ?1
+            """)
+    Optional<Integer> houresWorkedByEmployee(String id);
 }
