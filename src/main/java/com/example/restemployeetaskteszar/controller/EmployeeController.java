42e01e968a5992232919231dbe4abcfa6c3595bd
Dani4532 <d.teszar@htlstp.at>
2022-06-08 09:03:33 +0200

    getTasksFromTo getHouresWorked

--- src/main/java/com/example/restemployeetaskteszar/controller/EmployeeController.java
+++ src/main/java/com/example/restemployeetaskteszar/controller/EmployeeController.java
@@ -1,13 +1,17 @@
 package com.example.restemployeetaskteszar.controller;
 
 import com.example.restemployeetaskteszar.entity.Employee;
+import com.example.restemployeetaskteszar.entity.Task;
 import com.example.restemployeetaskteszar.repository.EmployeeRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -31,9 +35,43 @@ public class EmployeeController {
                 .orElseThrow(() -> new IllegalArgumentException());
     }
 
+    @GetMapping("/employees?name=partial_name")
+    public List<Employee> getAllThatContain(@PathVariable String name){
+        return repository.findEmployeesByFirstNameContaining(name);
+    }
+
+    @GetMapping("/employees/{id}/houresWorked")
+    public Integer getHouresWorked(@PathVariable String id){
+        var found = repository.findById(id);
+        var tasks = found.get().getTasks();
+        var sum = 0;
+        for (Task task : tasks) {
+            sum += task.getHouresWorked();
+        }
+        return sum;
+    }
+
+    @GetMapping("/employees/{id}/tsks?from=__&to=__")
+    public List<Task> getTasksFromTo(@PathVariable String id, @PathVariable String from, @PathVariable String to){
+        var found = repository.findById(id);
+        var tasks = found.get().getTasks();
+        return tasks.stream().filter(task ->{if (task.getFinished().isBefore(LocalDate.parse(to)) && task.getFinished().isAfter(LocalDate.parse(from))){
+            return true;
+        }
+            return false;
+        }).toList();
+    }
+
+
     @PostMapping("/employees")
     ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
         Employee saved = repository.save(employee);
-        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .build(saved.getId());
+        return ResponseEntity
+                .created(uri)
+                .body(saved);
     }
 }
